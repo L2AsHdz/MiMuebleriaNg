2b1f79975ab00e30c8abf946eed3f0ea00fa7283
DRM5314 <davidrodolfo-martinezmiranda@cunoc.edu.gt>
2024-03-27 19:20:20 -0600

    Feature: Add card for view in the sale

--- src/app/customer/component/sale/sale.component.ts
+++ src/app/customer/component/sale/sale.component.ts
@@ -3,6 +3,7 @@ import {FormBuilder, Validators} from "@angular/forms";
 import {MessageService} from "primeng/api";
 import {Router} from "@angular/router";
 import {CustomerService} from "../../service/customer.service";
+import {Customer} from "../../model/customer";
 
 @Component({
   selector: 'app-sale',
@@ -15,6 +16,7 @@ export class SaleComponent implements OnInit{
 
   @Input() nit!:string;
   @Input() sellingUser!:string;
+  customer!:Customer;
   saleForm = this.formMap.group({
     nit: [this.nit,Validators.required],
     sellingUserId: [this.sellingUser,Validators.required],
@@ -23,11 +25,15 @@ export class SaleComponent implements OnInit{
   constructor(private formMap:FormBuilder, private messageService:MessageService, private router:Router,private customerService:CustomerService) {
     // @ts-ignore
     this.nit = this.router.getCurrentNavigation()?.extras.state.nit;
-    console.log(this.nit);
   }
 
   ngOnInit() {
-
+    this.customerService.getCustomerByNit(this.nit).subscribe(
+      response=> {
+        this.customer = response;
+      },
+      error => this.messageService.add({severity:'error',summary:'Error',detail:error.error})
+    );
   }
 
   closeModal() {
